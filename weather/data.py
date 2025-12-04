5e49276034f08955cf5d15c2ddf684e886fd7aca
Isabel Suhr <isabels@ucar.edu>
2025-12-03 20:56:52 -0700

    Scrape temp from scott base website

--- weather/data.py
+++ weather/data.py
@@ -1,6 +1,9 @@
 import requests
+import bs4 
 
 boulder_url = "https://api.weather.gov/stations/KBDU/observations/latest"
+
+mcmurdo_url = "https://webmedia.antarcticanz.govt.nz/weather/sbweather.html"
 
 def c_to_f(c):
     return (c * 9./5) + 32
@@ -9,4 +12,13 @@ def boulder_temp():
     response = requests.get(boulder_url)
     data = response.json()
     temp = data["properties"]["temperature"]["value"]
+    return c_to_f(temp)
+
+def mcmurdo_temp():
+    # yeah ok it's scott base, but that's what I can scrape
+    response = requests.get(mcmurdo_url)
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    tstring = str(soup.find_all('td')[3].font.b.contents[0])
+    print(tstring, type(tstring))
+    temp = float(tstring.split("°")[0])
     return c_to_f(temp)
